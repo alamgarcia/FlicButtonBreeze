1714689e6fb956075717e3e980cd53422928ead6
Alam García Roldán <40043129+alamgarcia@users.noreply.github.com>
2019-07-18 12:09:04 -0500

    Android 8.0 Upgrgrade

--- app/src/main/java/com/example/flic/MainActivity.kt
+++ app/src/main/java/com/example/flic/MainActivity.kt
@@ -1,23 +1,21 @@
 package com.example.flic
 
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
-import android.os.AsyncTask
+import android.os.AsyncTask.execute
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import io.flic.lib.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
-import android.os.AsyncTask.execute
-import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.main.content_main.*
 import java.io.IOException
 
 
@@ -29,20 +27,13 @@ class MainActivity : AppCompatActivity() {
         var myPreferences = "myPrefs"
         var sharedPreferences = getSharedPreferences(myPreferences, Context.MODE_PRIVATE)
         var endpoint = sharedPreferences.getString("endpoint", "http://breeze2-132.collaboratory.avaya.com/services/EventingConnector/events")
-        var family = sharedPreferences.getString("family", "AAADEVRFID")
-        var type = sharedPreferences.getString("type", "AAADEVRFIDLOCALIZATION")
+        var family = sharedPreferences.getString("family", "AAADEVComiteEmergencia")
+        var type = sharedPreferences.getString("type", "AAADEVComiteEmergenciaType")
         var version = sharedPreferences.getString("version", "1.0")
-        var email = sharedPreferences.getString("email", "null")
-        var param1 = sharedPreferences.getString("param1", "null")
-        var param2 = sharedPreferences.getString("param2", "null")
-        var param3 = sharedPreferences.getString("param3", "null")
-        var param4 = sharedPreferences.getString("param4", "null")
-        var param5 = sharedPreferences.getString("param5", "null")
-        var param6 = sharedPreferences.getString("param6", "null")
-        var param7 = sharedPreferences.getString("param7", "null")
-        var param8 = sharedPreferences.getString("param8", "null")
-        var param9 = sharedPreferences.getString("param9", "null")
-        var param10 = sharedPreferences.getString("param10", "null")
+        var email = sharedPreferences.getString("nombreAdmin", "Alejandro")
+        var param1 = sharedPreferences.getString("numerosParticipantes", "2310,2311,2312,2314,2314,2316")
+        var param2 = sharedPreferences.getString("bridgePhone", "19728106902")
+        var param3 = sharedPreferences.getString("bridgeId", "903525552787969#")
         endpoint_txt.setText(endpoint)
         family_txt.setText(family)
         type_txt.setText(type)
@@ -51,13 +42,7 @@ class MainActivity : AppCompatActivity() {
         param1_txt.setText(param1)
         param2_txt.setText(param2)
         param3_txt.setText(param3)
-        param4_txt.setText(param4)
-        param5_txt.setText(param5)
-        param6_txt.setText(param6)
-        param7_txt.setText(param7)
-        param8_txt.setText(param8)
-        param9_txt.setText(param9)
-        param10_txt.setText(param10)
+
 
         Config().setFlicCredentials()
         try {
@@ -87,17 +72,10 @@ class MainActivity : AppCompatActivity() {
             editor.putString("family", family_txt.text.toString())
             editor.putString("type", type_txt.text.toString())
             editor.putString("version", version_txt.text.toString())
-            editor.putString("email", email_txt.text.toString())
-            editor.putString("param1", param1_txt.text.toString())
-            editor.putString("param2", param2_txt.text.toString())
-            editor.putString("param3", param3_txt.text.toString())
-            editor.putString("param4", param4_txt.text.toString())
-            editor.putString("param5", param5_txt.text.toString())
-            editor.putString("param6", param6_txt.text.toString())
-            editor.putString("param7", param7_txt.text.toString())
-            editor.putString("param8", param8_txt.text.toString())
-            editor.putString("param9", param9_txt.text.toString())
-            editor.putString("param10", param10_txt.text.toString())
+            editor.putString("nombreAdmin", email_txt.text.toString())
+            editor.putString("numerosParticipantes", param1_txt.text.toString())
+            editor.putString("bridgePhone", param2_txt.text.toString())
+            editor.putString("bridgeId", param3_txt.text.toString())
             editor.apply()
             val dialogBuilder = AlertDialog.Builder(this)
             dialogBuilder.setMessage("¿Deses probar tus ajustes ?")
@@ -107,25 +85,18 @@ class MainActivity : AppCompatActivity() {
                     var myPreferences = "myPrefs"
                     var sharedPreferences =  getSharedPreferences(myPreferences, Context.MODE_PRIVATE)
                     var endpoint = sharedPreferences.getString("endpoint", "http://breeze2-132.collaboratory.avaya.com/services/EventingConnector/events")
-                    var family = sharedPreferences.getString("family", "AAADEVRFID")
-                    var type = sharedPreferences.getString("type", "AAADEVRFIDLOCALIZATION")
+                    var family = sharedPreferences.getString("family", "AAADEVComiteEmergencia")
+                    var type = sharedPreferences.getString("type", "AAADEVComiteEmergenciaType")
                     var version = sharedPreferences.getString("version", "1.0")
-                    var email = sharedPreferences.getString("email", "null")
-                    var param1 = sharedPreferences.getString("param1", "null")
-                    var param2 = sharedPreferences.getString("param2", "null")
-                    var param3 = sharedPreferences.getString("param3", "null")
-                    var param4 = sharedPreferences.getString("param4", "null")
-                    var param5 = sharedPreferences.getString("param5", "null")
-                    var param6 = sharedPreferences.getString("param6", "null")
-                    var param7 = sharedPreferences.getString("param7", "null")
-                    var param8 = sharedPreferences.getString("param8", "null")
-                    var param9 = sharedPreferences.getString("param9", "null")
-                    var param10 = sharedPreferences.getString("param10", "null")
+                    var email = sharedPreferences.getString("nombreAdmin", "Alejandro")
+                    var param1 = sharedPreferences.getString("numerosParticipantes", "2310,2311,2312,2314,2314,2316")
+                    var param2 = sharedPreferences.getString("bridgePhone", "19728106902")
+                    var param3 = sharedPreferences.getString("bridgeId", "903525552787969#")
                     execute {
                         try {
                             val client = OkHttpClient()
                             val mediaType = MediaType.parse("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW")
-                            val body = RequestBody.create(mediaType, "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"family\"\r\n\r\n$family\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n$type\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"version\"\r\n\r\n$version\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"eventBody\"\r\n\r\n{'correoElectronico':'$email','Param1':'$param1','Param2':'$param2','Param3':'$param3','Param4':'$param4','Param5':'$param5','Param6':'$param6','Param7':'$param7','Param8':'$param8','Param9':'$param9','Param10':'$param10'} \r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
+                            val body = RequestBody.create(mediaType, "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"family\"\r\n\r\n$family\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n$type\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"version\"\r\n\r\n$version\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"eventBody\"\r\n\r\n{'nombreAdmin':'$email','numerosParticipantes':'$param1','bridgePhone':'$param2','bridgeId':'$param3'} \r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
                             val request = Request.Builder()
                                 .url(endpoint)
                                 .post(body)
@@ -206,26 +177,19 @@ class BroadcastReceiver : FlicBroadcastReceiver() {
             var myPreferences = "myPrefs"
             var sharedPreferences =  context.getSharedPreferences(myPreferences, Context.MODE_PRIVATE)
             var endpoint = sharedPreferences.getString("endpoint", "http://breeze2-132.collaboratory.avaya.com/services/EventingConnector/events")
-            var family = sharedPreferences.getString("family", "AAADEVRFID")
-            var type = sharedPreferences.getString("type", "AAADEVRFIDLOCALIZATION")
+            var family = sharedPreferences.getString("family", "AAADEVComiteEmergencia")
+            var type = sharedPreferences.getString("type", "AAADEVComiteEmergenciaType")
             var version = sharedPreferences.getString("version", "1.0")
-            var email = sharedPreferences.getString("email", "null")
-            var param1 = sharedPreferences.getString("param1", "null")
-            var param2 = sharedPreferences.getString("param2", "null")
-            var param3 = sharedPreferences.getString("param3", "null")
-            var param4 = sharedPreferences.getString("param4", "null")
-            var param5 = sharedPreferences.getString("param5", "null")
-            var param6 = sharedPreferences.getString("param6", "null")
-            var param7 = sharedPreferences.getString("param7", "null")
-            var param8 = sharedPreferences.getString("param8", "null")
-            var param9 = sharedPreferences.getString("param9", "null")
-            var param10 = sharedPreferences.getString("param10", "null")
+            var email = sharedPreferences.getString("nombreAdmin", "Alejandro")
+            var param1 = sharedPreferences.getString("numerosParticipantes", "2310,2311,2312,2314,2314,2316")
+            var param2 = sharedPreferences.getString("bridgePhone", "19728106902")
+            var param3 = sharedPreferences.getString("bridgeId", "903525552787969#")
             Toast.makeText(context, "Boton Presionado", Toast.LENGTH_SHORT).show()
             execute {
                 try {
                     val client = OkHttpClient()
                     val mediaType = MediaType.parse("multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW")
-                    val body = RequestBody.create(mediaType, "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"family\"\r\n\r\n$family\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n$type\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"version\"\r\n\r\n$version\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"eventBody\"\r\n\r\n{'correoElectronico':'$email','Param1':'$param1','Param2':'$param2','Param3':'$param3','Param4':'$param4','Param5':'$param5','Param6':'$param6','Param7':'$param7','Param8':'$param8','Param9':'$param9','Param10':'$param10'} \r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
+                    val body = RequestBody.create(mediaType, "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"family\"\r\n\r\n$family\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n$type\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"version\"\r\n\r\n$version\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"eventBody\"\r\n\r\n{'nombreAdmin':'$email','numerosParticipantes':'$param1','bridgePhone':'$param2','bridgeId':'$param3'} \r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
                     val request = Request.Builder()
                         .url(endpoint)
                         .post(body)
